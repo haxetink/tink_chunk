b3d4663b1b0a9efe8e8c6c5bfc04b6fe9821d4a0
Kevin Leung <kevinresol@gmail.com>
2019-12-25 00:26:47 +0800

    Update lib

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -2,13 +2,14 @@ package ;
 
 import tink.unit.*;
 import tink.testrunner.*;
+import tink.testrunner.Reporter;
 
 class RunTests {
   static function main() {
     Runner.run(TestBatch.make([
       new ChunkTest(),
       // new CursorTest(),
-    ]), new tink.testrunner.Reporter.CompactReporter()).handle(Runner.exit);
+    ]), new CompactReporter()).handle(Runner.exit);
   }
   
 }
